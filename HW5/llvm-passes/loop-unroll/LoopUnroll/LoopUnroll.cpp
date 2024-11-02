106f9ab43ee9f3b9b51c4c6945509ae1f964b6ae
jiangqucheng <jiangquchengpeter@hotmail.com>
2024-11-02 04:34:59 +0000

    #8 loop unroll pass cmake update

--- HW5/llvm-passes/loop-unroll/LoopUnroll/LoopUnroll.cpp
+++ HW5/llvm-passes/loop-unroll/LoopUnroll/LoopUnroll.cpp
@@ -8,6 +8,17 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "llvm/IR/Function.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Transforms/Utils/UnrollLoop.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/Analysis/AssumptionCache.h"
+#include "llvm/Analysis/TargetTransformInfo.h"
+
+
 using namespace llvm;
 
 #define __FILE_NAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
@@ -17,15 +28,34 @@ namespace {
 
 struct LoopUnrollPass : public PassInfoMixin<LoopUnrollPass> {
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+        errs() << __HERE__ << "Running Loop Unroll Pass on function: " << F.getName() << "\n";
+
         // Get loop analysis.
         LoopAnalysis::Result &LI = FAM.getResult<LoopAnalysis>(F);
 
+        // Get required analyses for UnrollLoop.
+        // auto &LI = FAM.getResult<LoopAnalysis>(F);
+        auto &SE = FAM.getResult<ScalarEvolutionAnalysis>(F);
+        auto &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+        auto &AC = FAM.getResult<AssumptionAnalysis>(F);
+        auto &TTI = FAM.getResult<TargetIRAnalysis>(F);
+        // auto &ORE = FAM.getResult<OptimizationRemarkEmitterAnalysis>(F);
+
         bool modified = false;
         for (Loop *L : LI) {
+            errs() << __HERE__ << "Found loop: " << *L << "\n";
+
             // Ensure the loop is in simplified form.
             if (L->isLoopSimplifyForm() && L->getLoopLatch()) {
-                // Unroll the loop by a factor of 2.
-                modified |= UnrollLoop(L, {2, /*AllowRuntime=*/false, /*AllowExpensiveTripCount=*/false});
+                errs() << __HERE__ << "Unrolling loop!\n";
+
+                // Configure unroll options
+                UnrollLoopOptions ULO;
+                ULO.Count = 2; // Unroll factor
+                ULO.Runtime = false;
+                ULO.AllowExpensiveTripCount = false;
+                
+                modified |= UnrollLoop(L, ULO, &LI, &SE, &DT, &AC, &TTI, nullptr, false, nullptr) != LoopUnrollResult::Unmodified;
             }
         }
 
@@ -41,11 +71,13 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginIn
         .PluginName = "LoopUnrollPass", 
         .PluginVersion = LLVM_VERSION_STRING,
         .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            errs() << "Registering LoopUnrollPass\n";
             // Register the pass with the "function-level" optimization pipeline.
             PB.registerPipelineParsingCallback(
                 [](StringRef Name, FunctionPassManager &FPM,
                     ArrayRef<PassBuilder::PipelineElement>) {
                     if (Name == "my-loop-unroll") {
+                        errs() << "Adding my-loop-unroll to the pipeline\n";
                         FPM.addPass(LoopUnrollPass());
                         return true;
                     }
